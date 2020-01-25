d51a86938d48115f66ac4dcb3c5ec02cfbefd2b4
Daniel Phillips <danphi@gmail.com>
2020-01-25 20:34:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  
+  data.each do |category, attributeArr|
+    attributeArr.each do |attribute, names|
+      names.each do |name|
+
+        # create name if doesn't exist
+        if !(pigeons.key?(name))
+          pigeons[name] = {}
+        end
+        
+        # create category if it doesn't exist
+        if (!pigeons[name].key?(category))
+          pigeons[name][category.to_sym] = []
+        end
+        
+        pigeons[name][category] << attribute.to_s
+      end
+    end
+  end
+
+  pigeons
 end
